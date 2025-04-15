fb109dee50b2e1956172817fed97f74fd7938ce9
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-04-15 14:03:04 +0400

    implement connection to database

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { startServer } from "./server";
-
+import { pool } from "./config/db";
 startServer();
 
+async function main() {
+  try {
+    const response = await pool.query("SELECT NOW()");
+    console.log("Database Connected:", response.rows[0].now);
+    startServer();
+  } catch (error) {
+    console.error("Error connecting to database", error);
+  }
+}
+
+main();
