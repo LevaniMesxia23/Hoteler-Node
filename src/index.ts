e87ea88da0e64ac04491c13c578a8f322c5456ef
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-04-15 13:39:13 +0400

    refactor starter code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
+import { startServer } from "./server";
+
+startServer();
 
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+export const startServer = () => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+};
