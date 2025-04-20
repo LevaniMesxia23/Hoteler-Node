fce8b5f00bdac8039b74a462a7161d1101f83d15
tornike2002 <toko.toka2000@gmail.com>
2025-04-20 08:11:22 -0700

    add routes for login register and getMe also add in server ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import authRoutes from "./routes/auth";
 dotenv.config();
 
 const app = express();
@@ -10,3 +10,5 @@ export const startServer = () => {
     console.log(`Server is running on port ${process.env.PORT}`);
   });
 };
+
+app.use("/api/auth", authRoutes);
